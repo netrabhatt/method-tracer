4ec6f0ef8b4cc80e3bae7aff194fdc07b7d412b8
Netra Bhatt <netra.bhatt@outlook.com>
2024-05-09 22:58:44 +1000

    Update Lineage.java

--- src/main/java/org/analyzer/Lineage.java
+++ src/main/java/org/analyzer/Lineage.java
@@ -7,47 +7,7 @@ public class Lineage {
 
     public static void main(String[] args) {
         // Example input data
-        String data = "com.task.domains.BullDozer.advance(int)#com.task.commands.AdvanceCommand.execute()\n" +
-                "com.task.commands.Command.execute()#com.task.commands.CommandRunner.runCommand(com.task.commands.Command)\n" +
-                "com.task.commands.CommandRunner.close()#com.task.commands.QuitCommand.execute()\n" +
-                "com.task.domains.BullDozer.left()#com.task.commands.TurnLeftCommand.execute()\n" +
-                "com.task.domains.BullDozer.right()#com.task.commands.TurnRightCommand.execute()\n" +
-                "com.task.domains.Position.getRow()#com.task.domains.BullDozer.requireBullDozerWithinBoundary()\n" +
-                "com.task.domains.BullDozer.clearCurrentCell()#com.task.domains.BullDozer.advance(int)\n" +
-                "com.task.domains.Position.getCol()#com.task.domains.BullDozer.requireBullDozerWithinBoundary()\n" +
-                "com.task.domains.GridObject.fuelUsage()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.GridObject.symbol()#com.task.domains.BullDozer.initDozerStats()\n" +
-                "com.task.domains.BullDozerStats.incrementProtectedTreesDestroyed()#com.task.domains.BullDozer.requireNoProtectedTreeVisit(com.task.domains.GridObject)\n" +
-                "com.task.domains.GridObject.symbol()#com.task.domains.BullDozer.clearCurrentCell()\n" +
-                "com.task.domains.BullDozer.requireBullDozerWithinBoundary()#com.task.domains.BullDozer.getCurrentObject()\n" +
-                "com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)#com.task.domains.BullDozer.advance(int)\n" +
-                "com.task.domains.Position.getRow()#com.task.domains.BullDozer.clearCurrentCell()\n" +
-                "com.task.domains.GridObject.resolveObject(char)#com.task.domains.BullDozer.getCurrentObject()\n" +
-                "com.task.domains.BullDozer.requireNoProtectedTreeVisit(com.task.domains.GridObject)#com.task.domains.BullDozer.getCurrentObject()\n" +
-                "com.task.domains.BullDozerStats.incrementCommOverhead()#com.task.domains.BullDozer.left()\n" +
-                "com.task.domains.BullDozerStats.setTotalCells(int)#com.task.domains.BullDozer.initDozerStats()\n" +
-                "com.task.domains.BullDozerStats.incrementPaintDamages()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.Position.getCol()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.Position.getCol()#com.task.domains.BullDozer.getCurrentObject()\n" +
-                "com.task.domains.BullDozerStats.incrementVisitedSquares()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.BullDozerStats.setProtectedTrees(int)#com.task.domains.BullDozer.initDozerStats()\n" +
-                "com.task.domains.Direction.left()#com.task.domains.BullDozer.left()\n" +
-                "com.task.domains.GridObject.isTree()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.Position.getRow()#com.task.domains.BullDozer.getCurrentObject()\n" +
-                "com.task.domains.Direction.right()#com.task.domains.BullDozer.right()\n" +
-                "com.task.domains.Position.getCol()#com.task.domains.BullDozer.clearCurrentCell()\n" +
-                "com.task.domains.BullDozer.getCurrentObject()#com.task.domains.BullDozer.advance(int)\n" +
-                "com.task.domains.Position.moveOneStep(com.task.domains.Direction)#com.task.domains.BullDozer.advance(int)\n" +
-                "com.task.domains.BullDozerStats.incrementCommOverhead()#com.task.domains.BullDozer.right()\n" +
-                "com.task.domains.BullDozerStats.updateFuelUsage(int)#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.GridObject.isProtectedTree()#com.task.domains.BullDozer.requireNoProtectedTreeVisit(com.task.domains.GridObject)\n" +
-                "com.task.domains.BullDozerStats.incrementCommOverhead()#com.task.domains.BullDozer.advance(int)\n" +
-                "com.task.domains.Position.getRow()#com.task.domains.BullDozer.updateDozerStats(boolean, com.task.domains.GridObject)\n" +
-                "com.task.domains.BullDozerStats.unclearedSquares()#com.task.domains.BullDozerStats.expenseResponse()\n" +
-                "com.task.domains.Grid.requireBullDozerWithinBoundary(int, int)#com.task.domains.Grid.next(com.task.domains.Direction, com.task.domains.Cell)\n" +
-                "com.task.domains.Cell.getCol()#com.task.domains.Grid.next(com.task.domains.Direction, com.task.domains.Cell)\n" +
-                "com.task.domains.Cell.getRow()#com.task.domains.Grid.next(com.task.domains.Direction, com.task.domains.Cell)\n" +
-                "com.task.domains.BullDozer.advance(int)#LAUDA";
+        String data = "";
         parseData(data);
 
         // Example: Get lineage of a specific method
